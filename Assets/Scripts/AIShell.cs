6caa96e222bfd960db79849e60062c20076e9b3c
Xihai Luo <xihai.luo01@gmail.com>
2023-01-24 23:20:09 -0500

    create auto attacking and tracking tank

---
+++ Assets/Scripts/AIShell.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIShell : MonoBehaviour
+{
+    public GameObject explosion;
+    Rigidbody rb;
+
+    void OnCollisionEnter(Collision col)
+    {
+
+        if (col.gameObject.tag == "tank")
+        {
+            GameObject exp = Instantiate(explosion, this.transform.position, Quaternion.identity);
+            Destroy(exp, 0.5f);
+            Destroy(this.gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.forward = rb.velocity;
+    }
+}
